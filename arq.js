4eb54ef4101898d48812a6873b2d36ab1310c140
Neponocena <pedronepo2005@gmail.com>
2026-01-30 16:43:37 -0300

    feat: Foi adicionado o input no lugar do alert

--- arq.js
+++ arq.js
@@ -1,50 +1,24 @@
-function lerNota(descricao) {
-      let nota = prompt(descricao);
+function calcular()
+    {
+        var np1 = Number(document.getElementById("np1").value)
+        var np2 = Number(document.getElementById("np2").value)
+        var pim = Number(document.getElementById("pim").value)
 
-      if (nota === null) {
-        alert("Operação cancelada");
-        return null;
-      }
+        var media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2);
 
-      nota = parseFloat(nota.replace(",", "."));
+        var res = document.getElementById("res")
 
-      if (isNaN(nota)) {
-        alert("Digite uma nota válida");
-        return null;
-      }
+        res.innerHTML = `Sua média semestral é ${media} `
 
-      if (nota < 0 || nota > 10) {
-        alert("A nota deve estar entre 0 e 10");
-        return null;
-      }
+        if(media >= 7)
+        {
+          res.innerHTML = `Sua média semestral é ${media} , Você esta aprovado!`
 
-      return nota;
-    }
-
-    function resultado() {
-      const np1 = lerNota("NOTA DA NP1:");
-      if (np1 === null) return;
-
-      const np2 = lerNota("NOTA DA NP2:");
-      if (np2 === null) return;
-
-      const pim = lerNota("NOTA DO PIM:");
-      if (pim === null) return;
-
-      const media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2);
-
-      if (media >= 7) {
-        alert(`Sua média é ${media.toFixed(2)}. Você está APROVADO!`);
-      } else {
-        alert(`Sua média é ${media.toFixed(2)}. Você está de EXAME!`);
-
-        const exame = lerNota("Digite a nota do exame:");
-        if (exame === null) return;
-
-        if (media + exame >= 10) {
-          alert("Você está aprovado!");
-        } else {
-          alert("Você está de DP!");
         }
-      }
+        else
+        {
+          res.innerHTML = `Sua média semestral é ${media} , Você esta de exame! `
+          
+        }
+        
     }
